ffcbfe292b73fdec2d62bb7517c83132577531d8
JhonyAMP <119316944+JhonyAMP@users.noreply.github.com>
2025-04-24 07:43:09 -0500

    Delete jam-libro-servicio directory

--- jam-libro-servicio/src/main/java/com/jam/libro/jamlibroservicio/Repositorio/LibroRepositorio.java
+++
@@ -1,6 +0,0 @@
-package com.jam.libro.jamlibroservicio.Repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepositorio extends JpaRepository<Libro, Long> {
-}
